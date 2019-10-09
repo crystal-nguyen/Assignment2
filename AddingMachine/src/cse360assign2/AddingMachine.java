2e8fee807c2c8788922a514777017e863739e489
crystalnguyen <crystalnguyen@crystals-mbp.3500.dhcp.asu.edu>
2019-10-08 21:52:15 -0700

    Final push (with methods)

--- AddingMachine/src/cse360assign2/AddingMachine.java
+++ AddingMachine/src/cse360assign2/AddingMachine.java
@@ -12,7 +12,8 @@ package cse360assign2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddingMachine {
+public class AddingMachine 
+{
 	
 	/**
 	 * Variable that keeps track of the total amount.
@@ -29,46 +30,55 @@ public class AddingMachine {
 	/**
 	 * Constructor for the class. 
 	 */
-	public AddingMachine() {
+	public AddingMachine() 
+	{
 		total = 0;		//not needed - included for clarity
 		history.add(String.valueOf(total));	//add the first total to history
 	}
 	
 	/**
-	 * Method that returns the total for the object created from this class.
-	 * @return returns the total attribute from this class.
+	 * Method that returns the total.
+	 * @return returns the total from the object created.
 	 */
-	public int getTotal() {
+	public int getTotal() 
+	{
 		return total;
 	}
 	
 	/**
 	 * Method that adds a number to total.
-	 * @param value to add.
+	 * @param value to add to total.
 	 */
-	public void add(int value) {
+	public void add(int value) 
+	{
 		total = total + value;
+		
+		//cast parameter to a string then add it and the operator to history
 		history.add(" + " + String.valueOf(value));
 	}
 	
 	/**
 	 * Method that subtracts a number from total.
-	 * @param value to subtract
+	 * @param value to subtract from total.
 	 */
-	public void substract (int value) {
+	public void subtract (int value) 
+	{
 		total = total - value;
-		history.add(" - " + String.valueOf(value));
 		
+		//cast parameter to a string then add it and the operator to history
+		history.add(" - " + String.valueOf(value));
 	}
 	
 	/**
-	 * Method that prints out attributes from this class.
-	 * @return returns a string of the class attributes.
+	 * Method that prints out history of transactions/calculations.
+	 * @return returns a string of the history of transactions/calculations.
 	 */
-	public String toString() {
+	public String toString() 
+	{
 		String result = "";
 		for (int startingIndex = 0; startingIndex < history.size(); startingIndex++)
 		{
+			//concatenate the element at startingIndex to result
 			result = result + history.get(startingIndex);
 		}
 		return result;
@@ -77,12 +87,10 @@ public class AddingMachine {
 	/**
 	 * Method that clears the total and the transaction history.
 	 */
-	public void clear() {
-		
-		//reset total and the history array list to 0
+	public void clear() 
+	{
 		total = 0;
 		history.clear();
 		history.add(String.valueOf(total));
 	}
-	
 }
