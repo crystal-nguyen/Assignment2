d5762bb50bfc75468f50b43607432de5215f8d27
crystalnguyen <crystal.nguyen@hotmail.com>
2019-10-08 18:18:47 -0700

    First push.

---
+++ AddingMachine/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,67 @@
+/* Name: Crystal Nguyen
+ * ID: 1212428007
+ * Class: CSE 360
+ * Assignment: 2
+ * Description: This class has methods that will perform arithmetic on
+ * 				numbers such as adding and subtracting. The class also keeps
+ * 				track of the total objects created and can clear the objects.
+ */
+
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	/**
+	 * Variable that keeps track of the total amount.
+	 */
+	private int total;
+	
+	
+	/**
+	 * Constructor for the class. 
+	 */
+	public AddingMachine() {
+		total = 0;	//not needed - included for clarity
+	}
+	
+	/**
+	 * Method that returns the total for the object created from this class.
+	 * @return returns the total attribute from this class.
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Method that adds a number to total.
+	 * @param value to add.
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Method that subtracts a number from total.
+	 * @param value to subtract
+	 */
+	public void substract (int value) {
+		
+	}
+	
+	/**
+	 * Method that prints out attributes from this class.
+	 * @return returns a string of the class attributes.
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Method that clears the total.
+	 */
+	public void clear() {
+	
+	}
+	
+}
